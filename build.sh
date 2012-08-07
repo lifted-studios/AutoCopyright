7deadd1fdd0b002eb5c6854d21aa222c1f092119
Lee Dohm <lee@liftedstudios.com>
2012-08-06 17:02:12 -0700

    Add to command palette.

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@
 package_dir="$HOME/Library/Application Support/Sublime Text 2/Packages/SublimeCopyright" 
 [ -d $package_dir ] || mkdir $package_dir
 cp *.py $package_dir
+cp *.sublime-* $package_dir
