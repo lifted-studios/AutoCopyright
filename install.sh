8ea38bf5f4f9649a7b8ac3945093a802c0495845
Lee Dohm <lee@liftedstudios.com>
2012-08-06 17:25:25 -0700

    Change the install script to bash.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
 # 
 # Copyright (c) 2012 by Lifted Studios.  All Rights Reserved.
 # 
 
-package_dir="$HOME/Library/Application Support/Sublime Text 2/Packages/SublimeCopyright" 
-[ -d $package_dir ] || mkdir $package_dir
-cp *.py $package_dir
-cp *.sublime-* $package_dir
+PACKAGE_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/SublimeCopyright" 
+[[ -d "$PACKAGE_DIR" ]] || mkdir "$PACKAGE_DIR"
+cp *.py "$PACKAGE_DIR"
+cp *.sublime-* "$PACKAGE_DIR"
