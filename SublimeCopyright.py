7a05070769b74f39e459e936025790c4e5a03837
Lee Dohm <lee@liftedstudios.com>
2012-08-11 15:21:16 -0700

    More refactoring to make the code simpler.

--- SublimeCopyright.py
+++ SublimeCopyright.py
@@ -39,35 +39,16 @@ class InsertCopyrightCommand(sublime_plugin.TextCommand):
     Executes the copyright command by inserting the appropriate copyright text at the current selection point.
     """
     try:
-      self.__get_block_comment_settings()
-      year = datetime.date.today().year
-      
-      owner = self.settings.get("owner")
-      if not owner:
-        raise MissingOwnerException()
-
-      location = self.__determine_location()
-      text = self.settings.get("copyright message")
-      text = text.replace("%y", str(year))
-      text = text.replace("%o", owner)
-
-      copyrightText = self.__build_block_comment(text)
-      self.view.insert(edit, location, copyrightText)
+      self.__insert_copyright(edit)
 
     except MissingOwnerException:
-      sublime.error_message("SublimeCopyright: Copyright owner not set")
-      user_settings_path = os.path.join(sublime.packages_path(), 'User', constants.SETTINGS_FILE)
-
-      if not os.path.exists(user_settings_path):
-        default_settings_path = os.path.join(sublime.packages_path(), 'SublimeCopyright', constants.SETTINGS_FILE)
-        shutil.copy(default_settings_path, user_settings_path)
-
-      sublime.active_window().open_file(user_settings_path)
+      self.__handle_missing_owner_exception()
 
   def __build_block_comment(self, text):
     """
     Builds a block comment and puts the given text into it.
     """
+    self.__get_block_comment_settings()
     endings = self.__get_line_endings()
 
     def make_comment(line): return self.middleLine + line + endings
@@ -89,6 +70,16 @@ class InsertCopyrightCommand(sublime_plugin.TextCommand):
       return region.end()
     else:
       return 0
+
+  def __format_text(self, year, owner):
+    """
+    Formats the text of the copyright message.
+    """
+    text = self.settings.get("copyright message")
+    text = text.replace("%y", str(year))
+    text = text.replace("%o", owner)
+
+    return text    
 
   def __get_block_comment_settings(self):
     """
@@ -117,3 +108,37 @@ class InsertCopyrightCommand(sublime_plugin.TextCommand):
     else:
       return u'\u000d'
 
+  def __get_owner(self):
+    """
+    Gets the copyright owner name that should be used in the copyright message.
+    """
+    owner = self.settings.get("owner")
+    if not owner:
+      raise MissingOwnerException()
+
+    return owner
+
+  def __handle_missing_owner_exception(self):
+    """
+    Opens the settings file and suggests the user edit it with the proper owner name.
+    """
+    sublime.error_message("Lifted Copyright: Copyright owner not set")
+    user_settings_path = os.path.join(sublime.packages_path(), 'User', constants.SETTINGS_FILE)
+
+    if not os.path.exists(user_settings_path):
+      default_settings_path = os.path.join(sublime.packages_path(), 'SublimeCopyright', constants.SETTINGS_FILE)
+      shutil.copy(default_settings_path, user_settings_path)
+
+    sublime.active_window().open_file(user_settings_path)
+
+  def __insert_copyright(self, edit):
+    """
+    Inserts the copyright message into the view.
+    """
+    year = datetime.date.today().year
+    owner = self.__get_owner()
+    location = self.__determine_location()
+    text = self.__format_text(year, owner)
+    copyrightText = self.__build_block_comment(text)
+
+    self.view.insert(edit, location, copyrightText)
