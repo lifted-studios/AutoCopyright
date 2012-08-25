69a00f52db2f1c48839d8035871937c3bf938503
Lee Dohm <lee@liftedstudios.com>
2012-08-24 21:09:37 -0700

    Add event listener to automatically update the copyright.

---
+++ AutoCopyrightEventListener.py
@@ -0,0 +1,17 @@
+# 
+# Copyright (c) 2012 by Lifted Studios.  All Rights Reserved.
+# 
+
+import sublime
+import sublime_plugin
+
+class AutoCopyrightEventListener(sublime_plugin.EventListener):
+  '''
+  Listener for application events.
+  '''
+  def on_pre_save(self, view):
+    '''
+    Called just before the view is saved.
+    '''
+    view.run_command('update_copyright')
+    
