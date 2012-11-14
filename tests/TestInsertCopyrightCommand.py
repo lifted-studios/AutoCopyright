84460e009e218509f38c1b0f1347748ad016c120
Lee Dohm <lee@liftedstudios.com>
2012-11-14 04:38:19 -0800

    Added test case for InsertCopyrightCommand class.

---
+++ tests/TestInsertCopyrightCommand.py
@@ -0,0 +1,19 @@
+# 
+# Copyright (c) 2012 by Lifted Studios.  All Rights Reserved.
+# 
+
+import os
+import sys
+
+test_dir = os.path.dirname(os.path.abspath(__file__))
+source_dir = os.path.dirname(test_dir)
+sys.path.append(test_dir)
+sys.path.append(source_dir)
+
+import unittest
+
+class TestInsertCopyrightCommand(unittest.TestCase):
+  pass
+
+if __name__ == "__main__":
+  unittest.main()
