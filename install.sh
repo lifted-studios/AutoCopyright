86c91b5dfb002d51387b21bcfd855c4672599c5f
Dohm, Lee <lcdohm@amazon.com>
2013-03-22 13:33:56 -0700

    Update script for ST3

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-PACKAGE_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/AutoCopyright"
+PACKAGE_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/AutoCopyright"
 
+rm *.pyc
 [[ -d $PACKAGE_DIR ]] && rm -rf $PACKAGE_DIR
 mkdir $PACKAGE_DIR
 cp * $PACKAGE_DIR
